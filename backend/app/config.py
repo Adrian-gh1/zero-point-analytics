50a75841e12b7f87542154ad84d275d41dce88db
Adrian-gh1 <Adrian22046@gmail.com>
2024-11-25 10:22:20 -0800

    Deployment Fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,8 @@
 import os
 
 class Configuration:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///dev.db')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///dev.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SCHEMA = os.environ.get('SCHEMA')
