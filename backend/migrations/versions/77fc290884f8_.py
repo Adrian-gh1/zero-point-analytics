061692b13ec50a753f2f28f9497cf1ca313328dd
Adrian-gh1 <Adrian22046@gmail.com>
2024-11-29 15:25:40 -0800

    Deployment Fix

--- backend/migrations/versions/77fc290884f8_.py
+++ backend/migrations/versions/77fc290884f8_.py
@@ -8,6 +8,9 @@ Create Date: 2024-11-29 15:20:58.200606
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '77fc290884f8'
@@ -44,6 +47,10 @@ def upgrade():
     sa.UniqueConstraint('username')
     )
     # ### end Alembic commands ###
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE businesses SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
 
 
 def downgrade():
