2a77a187b128d57c51abcbb9b2e16b5745485b76
Adrian-gh1 <Adrian22046@gmail.com>
2024-11-29 19:57:40 -0800

    Migration Fix

--- backend/migrations/versions/2d267086ad1b_.py
+++ backend/migrations/versions/2d267086ad1b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: bf6a5bfc535d
+Revision ID: 2d267086ad1b
 Revises: 
-Create Date: 2024-11-29 19:33:35.764489
+Create Date: 2024-11-29 19:56:15.183199
 
 """
 from alembic import op
@@ -13,7 +13,7 @@ environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
-revision = 'bf6a5bfc535d'
+revision = '2d267086ad1b'
 down_revision = None
 branch_labels = None
 depends_on = None
