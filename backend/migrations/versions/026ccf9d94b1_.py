572e2e7294658a62ef0d74c44f1e8be315c4be96
Adrian-gh1 <Adrian22046@gmail.com>
2024-12-02 04:01:17 -0800

    Dropped Deployment Schema and Re-Migrated

--- backend/migrations/versions/026ccf9d94b1_.py
+++ backend/migrations/versions/026ccf9d94b1_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 4e44a420434c
+Revision ID: 026ccf9d94b1
 Revises: 
-Create Date: 2024-12-02 03:50:59.398084
+Create Date: 2024-12-02 04:00:30.694525
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '4e44a420434c'
+revision = '026ccf9d94b1'
 down_revision = None
 branch_labels = None
 depends_on = None
